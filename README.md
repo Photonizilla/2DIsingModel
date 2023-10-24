4c8dd0aee94dc0a477b554ea27d3d7d17cec4106
Nick <79186371+Photonizilla@users.noreply.github.com>
2023-10-24 13:46:32 +0800

    SpacialAverage

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # 2DIsingModel
+
+$ d=2 $ Ising model describes a system with Hamiltonian
+
+$$ \beta H = - J \sum_{\left<i,j\right>}s_is_j - h \sum_i s_i $$
+
+where $ J > 0 $ for ferromagnets. The partition function is
+
+$$ Z = \sum_{\{s_i\}} \exp \left( J \sum_{\left<i,j\right>}s_is_j + h \sum_i s_i \right). $$
+
+Probability distribution function
+
+$$ p(\{s_i\}) = \frac{1}{Z} \mathrm{e}^{-H(\{s_i\})}. $$
+
+Let's investigate the expectation value of magnetic momentum $ M(\{s_i\}) = \frac{1}{N}\sum_i s_i $
+
+$$ \left<M\right> = \frac{1}{NZ} \sum_{\{s_i\}} M(\{s_i\}) \mathrm{e}^{-H(\{s_i\})}. $$
--- ising.cpp
+++ ising.cpp
@@ -6,7 +6,7 @@
 
 #define N 100
 
-double J=1.0, B, T=100, Z;
+double J=1.0, B=0.0, T=10, Z;
 double res[101];
 
 std::mt19937 ran((int)time(0));
@@ -20,13 +20,13 @@ bool Select() {
     return ((ran() % 2) == 1);	// Random true / false
 }
 
-void Generate(bool** s) {
+void Generate(bool** s) {		// Generate a random state
 	for(int i=1;i<=N;i++)
 		for(int j=1;j<=N;j++)
 			s[i][j] = Select();
 }
 
-double H(bool** s) {
+double H(bool** s) {			// Hamiltonian
 	double E = 0.0;
 	for(int i=1;i<=N;i++) {
 		for(int j=1;j<=N;j++) {
@@ -49,7 +49,7 @@ double Partition(bool** s, int i, int j, bool value) {
 	return Partition(s, i, j+1, true) + Partition(s, i, j+1, false);
 }
 
-double m(bool** s) {
+double m(bool** s) {			// Magnetic moment
 	double M = 0.0;
 	for(int i=1;i<=N;i++) {
 		for(int j=1;j<=N;j++) {
@@ -69,11 +69,11 @@ int main() {
 	}
 
 //	Z = Partition(s, 1, 1, true) + Partition(s, 1, 1, false);
-	printf("%f\n", Z);
+//	printf("%f\n", Z);
 
 	Generate(s);
 
-	for(int t=0;t<=50;t++) {
+	for(int t=0;t<=100;t++) {
 		bool** x = new bool* [N+1];
 		for(int i=1;i<=N;i++) {
 			x[i] = new bool [N+1];
@@ -86,8 +86,10 @@ int main() {
 			s = x;
 		res[t] = m(s);
 	}
-	for(int t=0;t<=50;t++)
-		printf("%f\n", res[t]);
+
+	freopen("ising.txt", "w", stdout);
+	for(int t=0;t<=100;t++)
+		printf("%7.4f\n", res[t]);
 
 	return 0;
 }
